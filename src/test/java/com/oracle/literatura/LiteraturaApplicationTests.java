fb347eb553694f234b54f74d3a0319629a6cd502
INRIAS <inrialva@gmail.com>
2024-12-10 22:22:36 -0500

    first commit

---
+++ src/test/java/com/oracle/literatura/LiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
